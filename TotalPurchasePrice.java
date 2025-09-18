f2266c4e4293cdb5b923280e442abc4b8f4b9c1b
komal upadhyay <komal@192.168.1.9>
2025-09-18 18:02:38 +0530

    assignment3

---
+++ TotalPurchasePrice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TotalPurchasePrice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter unit price: ");
+        double unitPrice = input.nextDouble();
+        System.out.print("Enter quantity: ");
+        int quantity = input.nextInt();
+        
+        double totalPrice = unitPrice * quantity;
+        System.out.printf("The total purchase price is INR %.2f if the quantity %d and unit price is INR %.2f\n", totalPrice, quantity, unitPrice);
+        input.close();
+    }
+}
